97ebd7ffa25753c19735db3092f3dfbb056dacba
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-01-17 09:50:33 +0530

    routers and config added

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.Mongo_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("✅DB Connection Succesfull..!");
-  })
-  .catch(() => {
-    console.log("❌DB Connection Failed..!");
-  });
+//importing database connnetion
+
+const connection = require("./utils/db");
+
+//importing routes
+
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+app.use(express.json());
+
+//DB connection
+connection();
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.get("/", (req, res) => {
   res.send("hello");
